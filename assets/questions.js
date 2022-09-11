02b367bcf353f87803b73d12c60c8d54883ff945
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-09-11 13:39:56 -0700

    Error changes to questions js file

--- assets/questions.js
+++ assets/questions.js
@@ -1,19 +1,19 @@
 var question = [
     {
-        numb: 1,
-        question: "Commonly used data types do NOT include..."
-        answer: "Alerts"
-        options: [
-            "Strings",
-            "Booleans",
-            "Alerts",
-            "Numbers"
+    numb: 1,
+    question: "Commonly used data types do NOT include...",
+    answer: "Alerts",
+    options: [
+        "Strings",
+        "Booleans",
+        "Alerts",
+        "Numbers"
         ]
     },
     {
         numb: 2,
-        question: "The condition in an if/else statement is enclosed within _____"
-        answer: "Curly Brackets"
+        question: "The condition in an if/else statement is enclosed within _____",
+        answer: "Curly Brackets",
         options: [
             "Quotes",
             "Curly Brackets",
@@ -23,8 +23,8 @@ var question = [
     },
     {
         numb: 3,
-        question: "Arrays in Javascript can be used to store _____"
-        answer: "All of the Above"
+        question: "Arrays in Javascript can be used to store _____",
+        answer: "All of the Above",
         options: [
             "Numbers and Strings",
             "Booleans",
@@ -34,8 +34,8 @@ var question = [
     },
     {
         numb: 4,
-        question: "String values must be enclosed within _____ when being assigned to variables"
-        answer: "Quotes"
+        question: "String values must be enclosed within _____ when being assigned to variables",
+        answer: "Quotes",
         options: [
             "Commas",
             "Curly Brackets",
@@ -45,8 +45,8 @@ var question = [
     },
     {
         numb: 5,
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:"
-        answer: "consoleLog"
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "consoleLog",
         options: [
             "consoleLog",
             "termina;/bash",
