5d687174680fb5e1435d757e71383287a25e86a7
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-09-11 13:16:41 -0700

    Updated HTML/CSS and added JS Questions

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+var question = [
+    {
+        numb: 1,
+        question: "Commonly used data types do NOT include..."
+        answer: "Alerts"
+        options: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if/else statement is enclosed within _____"
+        answer: "Curly Brackets"
+        options: [
+            "Quotes",
+            "Curly Brackets",
+            "Paranthesis",
+            "Square Brackets"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in Javascript can be used to store _____"
+        answer: "All of the Above"
+        options: [
+            "Numbers and Strings",
+            "Booleans",
+            "Other Arrays",
+            "All of the Above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within _____ when being assigned to variables"
+        answer: "Quotes"
+        options: [
+            "Commas",
+            "Curly Brackets",
+            "Quotes",
+            "Paranthesis"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:"
+        answer: "consoleLog"
+        options: [
+            "consoleLog",
+            "termina;/bash",
+            "javascript",
+            "for loops"
+        ]
+    },
+];
